2edd2c7150073a82da238550e520df1b5ca589c5
liyaocool <crazyliyao@163.com>
2019-10-14 17:39:49 +0800

    斐波那契数列;swing 下拉菜单,输入框,按钮综合事件

---
+++ MyTest.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * 斐波那契数列求和
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("您将求前几项的和:");
+        int n = scan.nextInt();
+        int sum = MyTest.jisuan(n);
+        System.out.println(sum);
+        ;
+    }
+
+    private static int jisuan(int n) {
+        int x = 1, y = 1, sum = 2, temp;
+        if (n <= 2) {
+            sum = 1;
+            return sum;
+        }
+        for (int i = 3; i <= n; i++) {
+            sum = sum + (x + y);
+            temp = x;
+            x = x + y;
+            y = temp;
+        }
+        return sum;
+    }
+
+}
+
--- swingTest/EventAction.java
+++ swingTest/EventAction.java
@@ -1,43 +1,82 @@
 package swingTest;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /**
- * 点击事件监听器
+ * 下拉选择器,输入框,按钮事件
  */
-public class EventAction {
+public class EventAction extends WindowAdapter implements ActionListener, ItemListener {
+
+    private JFrame frame = new JFrame("选择您的喜欢的水果");
+    private JPanel panel = new JPanel();
+    private JComboBox comboBox = new JComboBox(); //下拉选项
+    private JTextField textField = new JTextField(12); //输入框
+    private JLabel label = new JLabel("请在上方输入");
+    private JButton addBtn = new JButton("添加");
+    private JButton delBtn = new JButton("删除");
+
     public static void main(String[] args) {
+        EventAction e = new EventAction();
+        e.go();
+    }
 
-        JFrame frame = new JFrame("事件监听");
-        JPanel panel = new JPanel();
-        JButton button = new JButton("点击我");
-        JLabel label = new JLabel("");
-        GridLayout gridLayout = new GridLayout(2, 1, 5, 5);
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                {
-                    label.setText("按钮被单击了");
-                    System.out.println("按钮被单击了");
-                }
-            }
-        });
+    private void go() {
         frame.add(panel);
-        frame.setBounds(600, 300, 400, 400);
+        frame.setBounds(600, 300, 600, 100);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel.setLayout(gridLayout);
-        panel.add(button);
+        panel.add(comboBox);
+        panel.add(textField);
         panel.add(label);
+        panel.add(addBtn);
+        panel.add(delBtn);
 
+        comboBox.addItemListener(this);
+        comboBox.addItem("请选择");
+        comboBox.addItem("苹果");
 
+        label.setBounds(120, 50, 100, 40);
 
+        addBtn.addActionListener(this);
+        delBtn.addActionListener(this);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getSource());
+        String command = e.getActionCommand();
+        if (command.equals("添加")) {
+            String input = textField.getText();
+            if (input.equals("")) {
+                label.setText("不能输入为空");
+            } else {
+                comboBox.addItem(input);
+                label.setText("成功添加:" + input);
+            }
+
+        } else if (command.equals("删除")) {
+            String select = comboBox.getSelectedItem().toString();
+            if (select.equals("请选择")) {
+                label.setText("您还没有选择");
+            } else {
+                comboBox.removeItem(select);
+                label.setText("成功删除:" + select);
+            }
+        }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        String selItem = e.getItem().toString();
+        if (selItem.equals("请选择")) {
+            label.setText("您还没有选择");
+        } else {
+            label.setText("您选择了:" + selItem);
+        }
+    }
+
 
 }
 

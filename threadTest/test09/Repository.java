0239bfbcca73ad195acf631f8d67b9e026d82354
liyaocool <crazyliyao@163.com>
2019-10-17 23:17:49 +0800

    优化wait/notify/sleep使用

--- threadTest/test09/Repository.java
+++ threadTest/test09/Repository.java
@@ -13,36 +13,38 @@ public class Repository {
     public final int MAX_SIZE = 10;
 
     //增加货物方法，调用时，加入 对象互斥锁，其他线程不能同步操作
-    public synchronized void addGoods(Object obj) {
-        //while轮询，判别是否满仓，若满仓，则进入等待，释放对象互斥锁，其他线程可操作
-        while (goods.size() == MAX_SIZE) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                System.out.println("生产等待中。。。");
+    public void addGoods(Object obj) {
+        synchronized (this) {
+            //若满仓，则进入等待，释放对象互斥锁，其他线程可操作
+            while (goods.size() == MAX_SIZE) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("生产等待中。。。");
+                }
             }
+            //若跳过上方判断，说明没满仓，唤醒wait队列里第一个线程
+            goods.add(obj);
+            notify();
+            System.out.println(new Date() + " 已生产新货物。库存量：" + goods.size());
         }
-
-        //通过上方轮询，说明没满仓，唤醒该线程，即再次加上 对象互斥锁
-        notify();
-
-        goods.add(obj);
-        System.out.println(new Date() +" 已生产新货物。库存量：" + goods.size());
     }
 
     //消费货物方法
-    public synchronized void delGoods() {
-        while (goods.size() == 0) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                System.out.println("消费等待中。。。");
+    public void delGoods() {
+        synchronized (this) {
+            while (goods.size() == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+            goods.remove(0);
+            notify();
+            System.out.println(new Date() + " 已经消费货物。库存量：" + goods.size());
         }
 
-        notify();
 
-        goods.remove(0);
-        System.out.println(new Date()+" 已经消费货物。库存量：" + goods.size());
     }
 }

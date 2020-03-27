727c28ec60bc61c26d70aa1586965bac7d374c83
liyaocool <crazyliyao@163.com>
2020-03-27 10:27:46 +0800

    jdk 8 Strems 操作

--- strems/taskTest/RunTest.java
+++ strems/taskTest/RunTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -13,20 +14,20 @@ import java.util.stream.Collectors;
  */
 public class RunTest {
     public static void main(String[] args) {
-        Collection<TaskBean> taskBeans = Arrays.asList(
-                new TaskBean(1, "111"),
-                new TaskBean(1, "112"),
-                new TaskBean(1, "222"),
-                new TaskBean(2, "222"),
-                new TaskBean(3, "333")
-        );
-        //统计point为1的taskben总和
-        List<String> tasks = taskBeans.stream()
+        //TaskBean hashCode已经做过处理
+        TaskBean t1 = new TaskBean(1, "111");
+        TaskBean t2 = new TaskBean(1, "111");
+        TaskBean t3 = new TaskBean(1, "222");
+        TaskBean t4 = new TaskBean(2, "222");
+        TaskBean t5 = new TaskBean(3, "333");
+        List<TaskBean> taskBeans = Arrays.asList(t1, t2, t3, t4, t5);
+        System.out.println(t1.equals(t2));
+        System.out.println("========选出分数为1的，去除重复的，按照状态倒序的，前三个，归集为List========");
+        List<TaskBean> tasks = taskBeans.stream()
                 .filter(t -> t.getPoint() == 1)
-                .map(TaskBean::getStatus)
-                .limit(3)
                 .distinct()
-                .sorted()
+                .sorted(Comparator.comparing(TaskBean::getStatus).reversed())
+                .limit(3)
                 .collect(Collectors.toList());
 
         System.out.println(tasks);
@@ -43,8 +44,7 @@ public class RunTest {
                 .filter(t -> "222".equals(t.getStatus()))
                 .peek(t -> t.setStatus("999"))
                 .collect(Collectors.toList());
-        for (TaskBean taskBean:newTasks
-             ) {
+        for (TaskBean taskBean : newTasks) {
             System.out.println(taskBean.toString());
         }
     }

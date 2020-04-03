5075389416e544e03c4aed3835564936ddcde476
liyaocool <crazyliyao@163.com>
2020-04-03 14:33:30 +0800

    redis

--- redisTest/RedisJava.java
+++ redisTest/RedisJava.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class RedisJava {
     public static void main(String[] args) {
         //连接本地的 Redis 服务
-        Jedis jedis = new Jedis("39.105.31.154");
+        Jedis jedis = new Jedis("");
         jedis.auth("123456");
         //查看服务是否运行
         System.out.println("连接成功,服务正在运行: " + jedis.ping());
